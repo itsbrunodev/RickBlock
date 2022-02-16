519b671b79f371ecb4b0655ac6f507a7bec075cd
brunolepis <brunka215@gmail.com>
2022-02-16 19:32:06 +0100

    Initial Release

---
+++ content.js
@@ -0,0 +1,25 @@
+(() => {
+  const IDs = [
+    "dQw4w9WgXcQ",
+    "-51AfyMqnpI",
+    "oHg5SJYRHA0",
+    "cvh0nX08nRw",
+    "V-_O7nl0Ii0",
+    "2ocykBzWDiM",
+    "j5a0jTc9S10",
+    "HPk-VhRjNI8",
+  ];
+
+  chrome.storage.local.get(["disabled", "bypassed", "total"], (res) => {
+    if (!res.disabled && IDs.find((i) => location.href.includes(i))) {
+      chrome.storage.local.set({ total: (res["total"] || 0) + 1 });
+
+      if (!res.bypassed) {
+        location =
+          chrome.runtime.getURL("warn/warn.html") + "?" + location.href;
+      } else {
+        chrome.storage.local.set({ bypassed: false });
+      }
+    }
+  });
+})();
